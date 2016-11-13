6b917ebe613563e2e4fb92580656b620746af9ae
Michael De Abreu <michaeljota@outlook.com>
2016-11-13 02:37:41 -0400

    chore(vendor) import ngrx/core, ngrx/store and angular/compiler

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -11,9 +11,14 @@ import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
+import '@angular/compiler';
 import '@angular/forms';
 import '@angular/http';
 import '@angular/router';
+
+// ngrx
+import '@ngrx/core'
+import '@ngrx/store';
 
 // AngularClass
 import '@angularclass/hmr';
