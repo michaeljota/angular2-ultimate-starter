2b2dd6ca22b2fc2930a2788789f121fe3134784b
Michael De Abreu <michaeljota@outlook.com>
2016-11-26 14:47:32 -0400

    refactor(*): working development deploy

--- 1.8f4a48aa484b9935d035.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{1229:function(a,e){a.exports=[{res:"data"}]}});
---
+++ 1.chunk.js
@@ -0,0 +1,15 @@
+webpackJsonpac__name_([1],{
+
+/***/ "./src/assets/mock-data/mock-data.json":
+/***/ function(module, exports) {
+
+module.exports = [
+	{
+		"res": "data"
+	}
+];
+
+/***/ }
+
+});
+//# sourceMappingURL=1.map
